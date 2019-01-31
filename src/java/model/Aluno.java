821590c36b08b26e32f15f359fed49f5d54281af
Lucas Monteiro Ribeiro <luckas.ribeiro2015@gmail.com>
2019-01-31 12:04:56 -0200

    ok

---
+++ src/java/model/Aluno.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Aluno {
+    
+    private String Ra;
+    
+    ArrayList<Nota> n = new ArrayList<Nota>();
+}
---
+++ src/java/model/Nota.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Nota{
+    
+    private double p1;
+    private double p2;
+    private double l1;
+    private double l2;
+    private double media;
+    
+    public double getP1() {
+        return p1;
+    }
+    
+    public void setP1(double p1) {
+        this.p1 = p1;
+    }
+    
+    public double getP2() {
+        return p2;
+    }
+    
+    public void setP2(double p2) {
+        this.p2 = p2;
+    }
+    
+    public double getL1() {
+        return l1;
+    }
+    
+    public void setL1(double l1) {
+        this.l1 = l1;
+    }
+    
+    public double getL2() {
+        return l2;
+    }
+    
+    public void setL2(double l2) {
+        this.l2 = l2;
+    }
+    
+    public double getMedia() {
+        return media;
+    }
+    
+    public void setMedia(double media) {
+        this.media = media;
+    }
+}
